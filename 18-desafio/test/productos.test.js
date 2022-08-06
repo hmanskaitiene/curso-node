9d5b6b3332311c275460413457f664e4577e70eb
Hernan Anskaitiene <hmanskaitiene@teco.com.ar>
2022-08-06 16:12:32 -0300

    fix en test

--- 18-desafio/test/productos.test.js
+++ 18-desafio/test/productos.test.js
@@ -22,79 +22,79 @@ let cantidadProductos = 0;
 let productId = 0;
 
 describe('Pruebas de productos ',() => {
-	it('Obtener un estado 200 al pedir productos', (done) => {
-		chai.request(url)
-			.get('/api/productos')
-			.end( (err,res) => {
-				res.should.have.status(200);
-                res.body.should.be.a('array');
-                cantidadProductos = res.body.length
-				done();
-			});
-	});
+    it('Obtener un estado 200 al pedir productos', (done) => {
+        chai.request(url)
+        .get('/api/productos')
+        .end( (err,res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            cantidadProductos = res.body.length
+            done();
+        });
+    });
 
-	it('Obtener la cantidad de productos y esperar un array', (done) => {
-		chai.request(url)
-			.get('/api/productos')
-			.end( (err,res) => {
-				res.should.have.status(200);
-                res.body.should.be.a('array');
-                assert.strictEqual(res.body.length, cantidadProductos)
-				done();
-			});
-	});
-	it('Insertar un producto y esperar un objeto', (done) => {
-		chai.request(url)
-			.post('/api/productos')
-            .send(data)
-			.end( (err,res) => {
-				res.should.have.status(200);
-                res.body.should.be.a('object');
-                productId = res.body.id
-                cantidadProductos++;
-				done();
-			});
-	});
-	it('Validar que la cantidad de productos sea la correcta', (done) => {
-		chai.request(url)
-			.get('/api/productos')
-			.end( (err,res) => {
-				res.should.have.status(200);
-                res.body.should.be.a('array');
-                assert.strictEqual(res.body.length, cantidadProductos)
-				done();
-			});
-	});
-	it('Modificar un producto y ver el dato modificado', (done) => {
-		chai.request(url)
-			.put(`/api/productos/${productId}`)
-            .send(dataUpdated)
-			.end( (err,res) => {
-				res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.property('precio').eql('200');
-                res.body.should.have.property('nombre').eql('Producto de ejemplo modificado');
-				done();
-			});
-	});
-	it('Eliminar el producto', (done) => {
-		chai.request(url)
-			.delete(`/api/productos/${productId}`)
-			.end( (err,res) => {
-				res.should.have.status(200);
-                res.body.should.be.a('object');
-                cantidadProductos--;
-				done();
-			});
-	});
-	it('Validar que la cantidad de productos sea la correcta despues de la eliminación', (done) => {
-		chai.request(url)
-			.get('/api/productos')
-			.end( (err,res) => {
-				res.should.have.status(200);
-                res.body.should.be.a('array');
-                assert.strictEqual(res.body.length, cantidadProductos)
-				done();
-			});
-	});
+    it('Obtener la cantidad de productos y esperar un array', (done) => {
+        chai.request(url)
+        .get('/api/productos')
+        .end( (err,res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            assert.strictEqual(res.body.length, cantidadProductos)
+            done();
+        });
+    });
+    it('Insertar un producto y esperar un objeto', (done) => {
+        chai.request(url)
+        .post('/api/productos')
+        .send(data)
+        .end( (err,res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            productId = res.body.id
+            cantidadProductos++;
+            done();
+        });
+    });
+    it('Validar que la cantidad de productos sea la correcta', (done) => {
+        chai.request(url)
+        .get('/api/productos')
+        .end( (err,res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            assert.strictEqual(res.body.length, cantidadProductos)
+            done();
+        });
+    });
+    it('Modificar un producto y ver el dato modificado', (done) => {
+        chai.request(url)
+        .put(`/api/productos/${productId}`)
+        .send(dataUpdated)
+        .end( (err,res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('precio').eql('200');
+            res.body.should.have.property('nombre').eql('Producto de ejemplo modificado');
+            done();
+        });
+    });
+    it('Eliminar el producto', (done) => {
+        chai.request(url)
+        .delete(`/api/productos/${productId}`)
+        .end( (err,res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            cantidadProductos--;
+            done();
+        });
+    });
+    it('Validar que la cantidad de productos sea la correcta despues de la eliminación', (done) => {
+        chai.request(url)
+        .get('/api/productos')
+        .end( (err,res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            assert.strictEqual(res.body.length, cantidadProductos)
+            done();
+        });
+    });
 });
